d3b4c1c985f4bcfa64b00332cd34e07c34e5db5c
Alberto Leal <mail4alberto@gmail.com>
2020-05-05 12:59:43 -0400

    fetchEvent

--- src/sentry/static/sentry/app/views/performance/compare/fetchEvent.tsx
+++ src/sentry/static/sentry/app/views/performance/compare/fetchEvent.tsx
@@ -2,14 +2,47 @@ import React from 'react';
 
 import {Client} from 'app/api';
 import withApi from 'app/utils/withApi';
+import {Event} from 'app/types';
+
+type ChildrenProps = {
+  isLoading: boolean;
+  error: null | string;
+  event: Event | undefined;
+};
 
 type Props = {
   api: Client;
 
+  orgSlug: string;
   eventSlug: string;
 };
 
-class FetchEvent extends React.Component<Props> {
+type State = {
+  tableFetchID: symbol | undefined;
+} & ChildrenProps;
+
+class FetchEvent extends React.Component<Props, State> {
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const orgSlugChanged = prevProps.orgSlug !== this.props.orgSlug;
+    const eventSlugChanged = prevProps.eventSlug !== this.props.eventSlug;
+
+    if (orgSlugChanged || eventSlugChanged) {
+      this.fetchData();
+    }
+  }
+
+  fetchData() {
+    const {orgSlug, eventSlug} = this.props;
+
+    // note: eventSlug is of the form <project-slug>:<event-id>
+
+    const url = `/organizations/${orgSlug}/events/${eventSlug}/`;
+  }
+
   render() {
     return <div>FetchEvent</div>;
   }
