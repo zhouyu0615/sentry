57cb874e6fb65b09432d599492b7f439cbeb08ee
Alberto Leal <mail4alberto@gmail.com>
2020-05-05 10:08:16 -0400

    unmarshal event slugs

--- src/sentry/static/sentry/app/views/performance/compare/index.tsx
+++ src/sentry/static/sentry/app/views/performance/compare/index.tsx
@@ -6,16 +6,41 @@ import FetchEvent from './fetchEvent';
 
 type Props = {
   location: Location;
-  params: Params;
+  params: Partial<Params>;
 };
 
 class CompareContainer extends React.PureComponent<Props> {
+  getEventSlugs() {
+    let {baselineEventSlug, regressionEventSlug} = this.props.params;
+
+    baselineEventSlug =
+      typeof baselineEventSlug === 'string' ? baselineEventSlug.trim() : undefined;
+    regressionEventSlug =
+      typeof regressionEventSlug === 'string' ? regressionEventSlug.trim() : undefined;
+
+    return {
+      baselineEventSlug,
+      regressionEventSlug,
+    };
+  }
+
+  fetchEvent(eventSlug: string | undefined) {
+    if (!eventSlug) {
+      return null;
+    }
+
+    return <FetchEvent eventSlug={eventSlug} />;
+  }
+
   render() {
     console.log('props', this.props);
+
+    const {baselineEventSlug} = this.getEventSlugs();
+
     return (
       <div>
         <div>compare transactions</div>
-        <FetchEvent />
+        {this.fetchEvent(baselineEventSlug)}
       </div>
     );
   }
