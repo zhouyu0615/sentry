493cc5c1fa4a1333285999d15bbdffd24de7923b
Alberto Leal <mail4alberto@gmail.com>
2020-05-04 22:25:57 -0400

    feat(apm): Compare transactions

---
+++ src/sentry/static/sentry/app/views/performance/compare/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+class CompareContainer extends React.Component<Props> {
+  render() {
+    return <div>compare transactions</div>;
+  }
+}
+
+export default CompareContainer;
