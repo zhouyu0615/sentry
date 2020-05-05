1e9c91a90a7662c008bf05db3374070892ac032a
Alberto Leal <mail4alberto@gmail.com>
2020-05-05 08:50:23 -0400

    route stuff

--- src/sentry/static/sentry/app/views/performance/compare/index.tsx
+++ src/sentry/static/sentry/app/views/performance/compare/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Params} from 'react-router/lib/Router';
+import {Location} from 'history';
 
-type Props = {};
+type Props = {
+  location: Location;
+  params: Params;
+};
 
 class CompareContainer extends React.Component<Props> {
   render() {
+    console.log('props', this.props);
     return <div>compare transactions</div>;
   }
 }
