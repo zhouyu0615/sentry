ce5b2857bfad37df7565f8595d67d9abb710809c
Alberto Leal <mail4alberto@gmail.com>
2020-05-06 13:50:53 -0400

    check if given events are transactions

--- src/sentry/static/sentry/app/views/performance/compare/traceView.tsx
+++ src/sentry/static/sentry/app/views/performance/compare/traceView.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import {Event} from 'app/types';
+import {getTraceContext} from 'app/components/events/interfaces/spans/utils';
+
+import {isTransactionEvent} from './utils';
 
 type Props = {
   baselineEvent: Event;
@@ -9,6 +12,21 @@ type Props = {
 
 class TraceView extends React.Component<Props> {
   render() {
+    const {baselineEvent, regressionEvent} = this.props;
+
+    if (!isTransactionEvent(baselineEvent) || !isTransactionEvent(regressionEvent)) {
+      // TODO: better error
+      return <div>One of the given events is not a transaction.</div>;
+    }
+
+    const baselineTraceContext = getTraceContext(baselineEvent);
+    const regressionTraceContext = getTraceContext(regressionEvent);
+
+    if (!baselineTraceContext || !regressionTraceContext) {
+      // TODO: better error
+      return <div>There is no trace found in either of the given transactions.</div>;
+    }
+
     return <div>traceview</div>;
   }
 }
