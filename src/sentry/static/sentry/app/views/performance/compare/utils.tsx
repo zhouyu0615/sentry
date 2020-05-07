0454b978328a1e2b4f49673ebbafe18b307efb46
Alberto Leal <mail4alberto@gmail.com>
2020-05-07 19:38:54 -0400

    diffTransactions

--- src/sentry/static/sentry/app/views/performance/compare/utils.tsx
+++ src/sentry/static/sentry/app/views/performance/compare/utils.tsx
@@ -1,5 +1,6 @@
 import {SentryTransactionEvent} from 'app/types';
-import {ParsedTraceType} from 'app/components/events/interfaces/spans/types';
+import {RawSpanType} from 'app/components/events/interfaces/spans/types';
+import {parseTrace, generateRootSpan} from 'app/components/events/interfaces/spans/utils';
 
 export function isTransactionEvent(event: any): event is SentryTransactionEvent {
   if (!event) {
@@ -9,25 +10,59 @@ export function isTransactionEvent(event: any): event is SentryTransactionEvent 
   return event?.type === 'transaction';
 }
 
-// The diff state between a baseline span sub-tree and the regression span sub-tree
-type DiffState = 'no-changes' | 'new' | 'deleted';
-
-export type ComparisonReport = {
-  baselineTrace: ParsedTraceType;
-  regressionTrace: ParsedTraceType;
-};
+export type ComparisonReport = {};
 
 export function diffTransactions({
-  baselineTrace,
-  regressionTrace,
+  baselineEvent,
+  regressionEvent,
 }: {
-  baselineTrace: ParsedTraceType;
-  regressionTrace: ParsedTraceType;
+  baselineEvent: SentryTransactionEvent;
+  regressionEvent: SentryTransactionEvent;
 }): ComparisonReport {
-  const report = {
-    baselineTrace,
-    regressionTrace,
-  };
+  const baselineTrace = parseTrace(baselineEvent);
+  const regressionTrace = parseTrace(regressionEvent);
+
+  const spans = [];
+
+  // merge the two transaction's span trees
+
+  const baseRootSpan: RawSpanType = generateRootSpan(baselineTrace);
+
+  // we maintain a stack of spans to be compared
+  const spansToBeCompared: Array<{
+    baselineSpan: RawSpanType;
+    regressionSpan: RawSpanType;
+  }> = [
+    {
+      baselineSpan: generateRootSpan(baselineTrace),
+      regressionSpan: generateRootSpan(regressionTrace),
+    },
+  ];
+
+  while (spansToBeCompared.length > 0) {
+    const currentSpans = spansToBeCompared.pop();
+
+    if (!currentSpans) {
+      break;
+    }
+
+    compareSpans(currentSpans);
+  }
+
+  const report = {};
 
   return report;
+}
+
+function compareSpans({
+  baselineSpan,
+  regressionSpan,
+}: {
+  baselineSpan: RawSpanType;
+  regressionSpan: RawSpanType;
+}) {
+  console.log({
+    baselineSpan,
+    regressionSpan,
+  });
 }
